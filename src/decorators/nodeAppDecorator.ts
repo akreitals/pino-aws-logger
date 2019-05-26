0a710bdf82eaab36d78b75ceb30b23366aa89ab5
Adam Kreitals <adam.kreitals@gmail.com>
2019-05-26 17:12:34 +1000

    fix(nodeAppDecorator): Disable if no package.json
    Disable the decorator is no package.json can be found i.e. on lambda

--- src/decorators/nodeAppDecorator.ts
+++ src/decorators/nodeAppDecorator.ts
@@ -1,6 +1,4 @@
 import { LogDecorator } from './logDecoratorInterface';
-// tslint:disable-next-line:no-var-requires
-const { name, version } = require('root-require')('package.json');
 
 export interface NodeAppDecorator extends LogDecorator {
   getMetadata: () => NodeAppMetadata;
@@ -11,14 +9,45 @@ interface NodeAppMetadata {
   version: string;
 }
 
-const isEnabled = (): Promise<boolean> => Promise.resolve(true);
+interface PackageJSON {
+  name: string;
+  version: string;
+  [key: string]: any;
+}
+
+function loadMainPackageJSON(attempts: number = 1): PackageJSON {
+  if (attempts > 5) {
+    throw new Error("Can't resolve main package.json file");
+  }
+
+  const mainPath = attempts === 1 ? './' : Array(attempts).join('../');
+  try {
+    // @ts-ignore
+    return require.main.require(mainPath + 'package.json');
+  } catch (e) {
+    return loadMainPackageJSON(attempts + 1);
+  }
+}
+
+const isEnabled = (): Promise<boolean> => {
+  try {
+    loadMainPackageJSON();
+    return Promise.resolve(true);
+  } catch (error) {
+    return Promise.resolve(false);
+  }
+};
 
 const metadataKey = 'app';
 
-const getMetadata = (): NodeAppMetadata => ({
-  name,
-  version,
-});
+const getMetadata = (): NodeAppMetadata => {
+  const { name, version } = loadMainPackageJSON();
+
+  return {
+    name,
+    version,
+  };
+};
 
 const decorator: NodeAppDecorator = {
   isEnabled,
